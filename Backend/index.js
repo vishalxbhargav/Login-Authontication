65f4530b2c8e4f89b22faec9636a2d8900e270c7
vishalxbhargav <vishalxbhargav@gmail.com>
2023-12-14 16:01:59 +0530

    create react app

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,10 @@
-const http=require('http');
-const fs =require('fs');
+const express=require('express');
 
-const myserver=http.createServer((req,res)=>{
-    const log=`${Date.now()}:${req.url} New Req Received`;
-    fs.appendFile('./log.txt',log,(err,data)=>{
-        switch(req.url){
-            case "/":res.end("HomePage");
-            break
-            case "/about":res.end("Vishal Bhargav");
-            break
-            case "/work":res.end("Developer");
-            break
-            default:res.end("404");
-        }
-        
-    });
-   
-});
-myserver.listen(3000,()=>{
-    console.log("seerver sstartted");
-});
+const app=express();
+app.listen(3000,(req,res)=>{
+    console.log("server is started");
+})
 
+app.get('/',(res,data)=>{
+    data.send("API is Active");
+})
